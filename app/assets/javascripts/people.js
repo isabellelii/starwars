eaabf5297d045c8f1810d41fcc6f70170abcbea1
Isabelle <isabellelidberg@live.se>
2018-04-30 11:22:10 +0200

    adds people list to people page

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -1,42 +1,55 @@
-function getInfo() {
+document.addEventListener("DOMContentLoaded", function(){
+  getInfo();
+});
 
-  updateWithLoading()
+function getInfo() {
   let apiURL = 'https://swapi.co/api/people/'
 
-  axios.get(apiURL).then(response => {
-    console.log(response.data)
-    updateInfo(response.data)
-  }).catch(e => {
+  fetch(apiURL).then(function (response) {
+      response.json().then(function(json){
+         appendUsersToSection(json.results)
+      })
+  }).catch(function(error) {
     updateInfoWithError()
   })
 }
 
-function updateInfo(data) {
-  let name = document.querySelector("#name");
-  let height = document.querySelector("#height");
-  let mass = document.querySelector("#mass");
-  let eyeColor = document.querySelector("#eye_color");
-  let homeworld = document.querySelector("#homeworld");
-  let vehicles = document.querySelector("#vehicles");
-
-  data.forEach(function(person) {
-    name.innerText = data.name
-    height.innerText = `Height: ${data.height}`
-    mass.innerText = `Mass: ${data.mass}`
-    eyeColor.innerText = `Eye Color: ${data.eye_color}`
-    homeworld.innerHTML = `<a href="${data.homeworld}">Homeworld</a></br>`
-    vehicles.innerHTML = `<a href="${data.vehicles}">Vehicles</a>`
-  }) 
+function appendUsersToSection(characters) {
+    const element = document.getElementById('peoplelist');
+    characters.forEach(function(character) {
+        formatUserDiv(character).then(function(formattedHtml){
+            let div = document.createElement('div');
+            let html = formattedHtml;
+            div.innerHTML = html;
+            element.appendChild(div);
+        })
+    })
 }
 
-function updateInfoWithError() {
-  let name = document.querySelector("#name");
-  name.innerText = 'Looks like this person isnt available right now!'
+function formatUserDiv(character) {
+    return new Promise(function(resolve){
+        getHomeworld(character.homeworld)
+        .then(function(homeworld){
+            let html = `<h2> ${character.name}</h2>`;
+            html += `<p>Height: ${character.height}</p>`;
+            html += `<p>Mass: ${character.mass}</p>`;
+            html += `<p>Eye Color: ${character.eye_color}</p>`
+            html += `<p>Homeworld: ${homeworld}</p>`
+            console.log(html)
+            resolve(html);
+        })
+    })
+
 
 }
 
-function updateWithLoading() {
-  let name = document.querySelector("#name");
-  name.innerHTML = '<i class="fas fa-spinner"></i>'
+function getHomeworld(url) {
+    return new Promise(function(resolve){
+        fetch(url).then(function(response){
+        response.json().then(function(json){
+            resolve(json.name);
+        })
+    })
+    })
 
 }
