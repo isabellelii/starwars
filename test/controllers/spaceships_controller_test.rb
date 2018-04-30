c9623c61b76aebc64524a7001fbaca76a75a9d68
Isabelle <isabellelidberg@live.se>
2018-04-30 16:10:31 +0200

    remove controller tests

--- test/controllers/spaceships_controller_test.rb
+++ test/controllers/spaceships_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class SpaceshipsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get spaceships_index_url
-    assert_response :success
-  end
+
 
 end
